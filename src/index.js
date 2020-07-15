3ba6f5fc5f73c4374633f266c2112764af61a39a
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-15 16:00:50 +0300

    added reducers to auth, not completed

--- src/index.js
+++ src/index.js
@@ -9,16 +9,21 @@ import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import burgerReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
+import authReducer from "./store/reducers/auth";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerReducer,
   order: orderReducer,
+  auth: authReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 const app = (
   <Provider store={store}>

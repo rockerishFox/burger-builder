66104cbc4af79b53d35c18e09c9adbceb0d3ca4e
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-15 12:08:23 +0300

    Update order.js
    - added reducer that handles succes and error cases when ordering a burger

---
+++ src/store/reducers/order.js
@@ -0,0 +1,27 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+  orders: [],
+  loading: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.PURCHASE_BURGER_SUCCES:
+      return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat({
+          ...action.orderData,
+          id: action.orderId,
+        }),
+      };
+
+    case actionTypes.PURCHASE_BURGER_ERROR:
+      return { ...state, loading: false };
+    default:
+      return state;
+  }
+};
+
+export default reducer;

de9e08619adba61368215b5e489c184e64fa2955
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-15 13:55:02 +0300

    Update actions/order.js
    - corrected dispatching "purchaseBurgerSuccess" function

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,7 +28,7 @@ export const purchaseBurger = (orderData) => {
     axios
       .post("/orders.json", orderData)
       .then((response) => {
-        dispatch(purchaseBurgerStart(response.data, orderData));
+        dispatch(purchaseBurgerSuccess(response.data.name, orderData));
       })
       .catch((error) => {
         dispatch(purchaseBurgerError(error));

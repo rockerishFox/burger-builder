a5e80f981707ba3cef4ca24ddcc2e1253a94d544
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-16 13:29:02 +0300

    add route protection using guards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./containers/Layout/Layout";
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -12,27 +12,48 @@ import { connect } from "react-redux";
 import * as actions from "./store/actions/index";
 
 class App extends Component {
-
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
 
   render() {
-    return (
-      <div>
-        <Layout>
+    let routes = (
+      <React.Fragment>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      </React.Fragment>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <React.Fragment>
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
             <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/" />
           </Switch>
-        </Layout>
+        </React.Fragment>
+      );
+    }
+
+    return (
+      <div>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -40,4 +61,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);

062603bea6004c36f8817e7ea608665392c5c2f9
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-16 20:37:54 +0300

    Create reducer/auth.test.js

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,40 @@
+import reducer from "./auth";
+
+import * as actionTypes from "../actions/actionTypes";
+
+describe("auth reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: "/",
+    });
+  });
+
+  it("should store token upon log in", () => {
+    expect(
+      reducer(
+        {
+          token: null,
+          userId: null,
+          error: null,
+          loading: false,
+          authRedirectPath: "/",
+        },
+        {
+          type: actionTypes.AUTH_SUCCESS,
+          idToken: "some-token",
+          userId: "some-id",
+        }
+      )
+    ).toEqual({
+      token: "some-token",
+      userId: "some-id",
+      error: null,
+      loading: false,
+      authRedirectPath: "/",
+    });
+  });
+});

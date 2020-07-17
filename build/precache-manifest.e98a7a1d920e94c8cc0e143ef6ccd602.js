0707503e223646e871e4e517fa044fdd615cc2ab
RockerishFox <57776990+rockerishFox@users.noreply.github.com>
2020-07-17 10:15:21 +0300

    created build directory for deploying

---
+++ build/precache-manifest.e98a7a1d920e94c8cc0e143ef6ccd602.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1243772803a581bc0bc048f0e87e2b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86c6215e75353c962556",
+    "url": "/static/css/3.69c03e01.chunk.css"
+  },
+  {
+    "revision": "c9ceaad806fe00106146",
+    "url": "/static/css/4.82ef66b5.chunk.css"
+  },
+  {
+    "revision": "ab8a4ee95a10b26aa15d",
+    "url": "/static/css/5.03b6873b.chunk.css"
+  },
+  {
+    "revision": "201895e802c80995bf4b",
+    "url": "/static/css/main.ac4f5a64.chunk.css"
+  },
+  {
+    "revision": "59d6e8db6f67c1ab7748",
+    "url": "/static/js/2.975979d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.975979d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86c6215e75353c962556",
+    "url": "/static/js/3.2eeb19dc.chunk.js"
+  },
+  {
+    "revision": "c9ceaad806fe00106146",
+    "url": "/static/js/4.21eb82e1.chunk.js"
+  },
+  {
+    "revision": "ab8a4ee95a10b26aa15d",
+    "url": "/static/js/5.5f3b0af9.chunk.js"
+  },
+  {
+    "revision": "201895e802c80995bf4b",
+    "url": "/static/js/main.c71ad1ff.chunk.js"
+  },
+  {
+    "revision": "8f79a3526c40e6fb7a2c",
+    "url": "/static/js/runtime-main.c753f4a5.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
